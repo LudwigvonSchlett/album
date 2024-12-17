1281bd343d7f15150742de944e2d5644e232b3b8
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2024-12-17 08:46:03 +0100

    main-view.fxml

--- src/main/java/ensisa/album/AlbumController.java
+++ src/main/java/ensisa/album/AlbumController.java
@@ -1,6 +1,26 @@
 package ensisa.album;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 public class AlbumController {
 
+
+    public AlbumController() {
+
+    }
+
+    public void initialize() {
+
+    }
+
+    @FXML
+    public Pane testPane;
+
+    @FXML
+    private void quitMenuAction() {
+        Platform.exit();
+    }
 
 }
