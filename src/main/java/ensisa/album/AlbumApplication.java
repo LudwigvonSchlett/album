78b4cb9cd027dbd72276f9f263dfdd19c1326a88
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2024-12-17 08:32:10 +0100

    main-view.fxml

--- src/main/java/ensisa/album/AlbumApplication.java
+++ src/main/java/ensisa/album/AlbumApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class AlbumApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AlbumApplication.class.getResource("Album.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AlbumApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Album");
         stage.setScene(scene);
