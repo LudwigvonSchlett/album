206910a42d0c154b68a8a494fd9dec4d0bf8a944
SombraSwiftwind <maxence.desmonteix@uha.fr>
2025-01-10 11:25:01 +0100

    Modification des actions imageBackwardsBackground & imageForwardsBackground en UndoableCommand pour que le undo marche

--- src/main/java/ensisa/album/command/ImageBackgroundBackwardCommand.java
+++ src/main/java/ensisa/album/command/ImageBackgroundBackwardCommand.java
@@ -2,47 +2,42 @@ package ensisa.album.command;
 
 import ensisa.album.AlbumController;
 import ensisa.album.model.ImageModel;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
-import java.util.List;
 
 public class ImageBackgroundBackwardCommand implements UndoableCommand {
     private AlbumController controller;
-    private ImageModel imageToMoveBackward;
-    private ObservableList<ImageModel> allImages;
-    private int indexAllImages;
-    private ObservableList<ImageModel> allImagesUndo;
+    private ImageModel imageToMoveBackward; // Image à déplacer
+    private ObservableList<ImageModel> allImages; // Liste des images
+    private int indexAllImages; // Index de l'image à déplacer
+    private ObservableList<ImageModel> allImagesUndo; // Snapshot de la liste initiale
 
     public ImageBackgroundBackwardCommand(ObservableList<ImageModel> allImages, int indexAllImages, ImageModel imageToMoveBackward, AlbumController controller) {
         this.imageToMoveBackward = imageToMoveBackward;
         this.controller = controller;
         this.allImages = allImages;
         this.indexAllImages = indexAllImages;
-        this.allImagesUndo = allImages;
+        this.allImagesUndo = FXCollections.observableArrayList(allImages);
     }
 
     @Override
     public void undo() {
         controller.deselectAll();
-        new ImageBackgroundForwardCommand(allImages, indexAllImages - 1, imageToMoveBackward, controller).execute();
 
-        System.out.println("Click on ImageBackgroundBackwardCommand undo");
-        controller.getUndoRedoHistory().printStacks();
-        //        ImageModel nextImage = allImages.get(indexAllImages);
-//        allImages.set(indexAllImages, imageToMoveBackward);
-//        allImages.set(indexAllImages - 1, nextImage);
+        allImages = FXCollections.observableArrayList(allImagesUndo);
+
+        controller.getDocument().getImages().setAll(allImages);
     }
 
     @Override
     public void execute() {
         controller.deselectAll();
+
         // Echange avec l'image précédente
         ImageModel previousImage = allImages.get(indexAllImages - 1);
         allImages.set(indexAllImages - 1, imageToMoveBackward);
         allImages.set(indexAllImages, previousImage);
 
         controller.getDocument().getImages().setAll(allImages);
-        System.out.println("Click on ImageBackgroundBackwardCommand");
-        controller.getUndoRedoHistory().printStacks();
     }
 }
--- src/main/java/ensisa/album/command/ImageBackgroundForwardCommand.java
+++ src/main/java/ensisa/album/command/ImageBackgroundForwardCommand.java
@@ -2,48 +2,42 @@ package ensisa.album.command;
 
 import ensisa.album.AlbumController;
 import ensisa.album.model.ImageModel;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
-import java.util.List;
 
 public class ImageBackgroundForwardCommand implements UndoableCommand {
     private AlbumController controller;
-    private ImageModel imageToMoveForward;
-    private ObservableList<ImageModel> allImages;
-    private ObservableList<ImageModel> allImagesUndo;
-    private int indexAllImages;
+    private ImageModel imageToMoveForward; // Image à déplacer
+    private ObservableList<ImageModel> allImages; // Liste des images
+    private ObservableList<ImageModel> allImagesUndo; // Snapshot de la liste initiale
+    private int indexAllImages; // Index de l'image à déplacer
 
     public ImageBackgroundForwardCommand(ObservableList<ImageModel> allImages, int indexAllImages, ImageModel imageToMoveForward, AlbumController controller) {
         this.imageToMoveForward = imageToMoveForward;
         this.controller = controller;
         this.allImages = allImages;
         this.indexAllImages = indexAllImages;
-        this.allImagesUndo = allImages;
+        this.allImagesUndo = FXCollections.observableArrayList(allImages);
     }
 
     @Override
     public void undo() {
         controller.deselectAll();
-        new ImageBackgroundBackwardCommand(allImages, indexAllImages + 1, imageToMoveForward, controller).execute();
-        System.out.println("CLick on ImageBackgroundForwardCommand undo");
 
-        controller.getUndoRedoHistory().printStacks();
-        //        ImageModel previousImage = allImages.get(indexAllImages);
-//        allImages.set(indexAllImages, imageToMoveForward);
-//        allImages.set(indexAllImages + 1, previousImage);
+        allImages = FXCollections.observableArrayList(allImagesUndo);
+
+        controller.getDocument().getImages().setAll(allImages);
     }
 
     @Override
     public void execute() {
         controller.deselectAll();
+
         // Échange avec l'image suivante
         ImageModel nextImage = allImages.get(indexAllImages + 1);
         allImages.set(indexAllImages + 1, imageToMoveForward);
         allImages.set(indexAllImages, nextImage);
 
         controller.getDocument().getImages().setAll(allImages);
-
-        System.out.println("Click on ImageBackgroundForwardCommand");
-        controller.getUndoRedoHistory().printStacks();
     }
 }
